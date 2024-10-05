9d80b2f0131b9999e29fae3d6567745d85346ae6
Samarth Vaidya <v.samarth1212@gmail.com>
2024-10-05 11:55:25 +0530

    Added navbar and spotlight

--- QuizQuestFrontend/src/App.jsx
+++ QuizQuestFrontend/src/App.jsx
@@ -1,15 +1,44 @@
 import "./App.css";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import FloatingNav from "./components/navbar";
+import { IoMdHome, IoMdAddCircle, IoMdMail } from "react-icons/io";
+
 function App() {
+  const navItems = [
+    {
+      name: "HOME",
+      link: "/",
+      icon: <IoMdHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "ABOUT",
+      link: "/",
+      icon: (
+        <IoMdAddCircle className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+    {
+      name: "CREATE",
+      link: "/",
+      icon: <IoMdMail className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "JOIN",
+      link: "/",
+      icon: <IoMdMail className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+  ];
+
   return (
-    <>
+    <div className="p-0 m-0">
       <BrowserRouter>
+        <FloatingNav navItems={navItems} />
         <Routes>
           <Route path="/" element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
