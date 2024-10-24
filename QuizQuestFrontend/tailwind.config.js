e5e3892f3883a46658eda53f71a00d4055616ae0
Bhumika <sharmabhmi@gmail.com>
2024-10-24 17:06:35 +0530

    Profile Crad enhanced

--- QuizQuestFrontend/tailwind.config.js
+++ QuizQuestFrontend/tailwind.config.js
@@ -32,6 +32,9 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        'nunito': ['"Nunito Sans"', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
