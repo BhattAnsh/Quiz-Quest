7160087607a0423160f888c4d4dcd16d1a49c554
harshbhar0629 <harshbhar.0629@gmail.com>
2024-10-30 18:26:39 +0530

    Overflow hide in x-direction

--- QuizQuestFrontend/src/App.jsx
+++ QuizQuestFrontend/src/App.jsx
@@ -86,7 +86,7 @@ function App() {
   const shouldDisplayNavbar = !routesWithoutNavbar.includes(location.pathname);
 
   return (
-    <div className="p-0 m-0">
+    <div className="p-0 m-0 overflow-x-hidden">
       {shouldDisplayNavbar && <FloatingNav navItems={navItems} />}
       {routing}
       <Footer></Footer>
