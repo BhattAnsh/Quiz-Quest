f6c1398740226055447fa211bd022e526f0f1ff0
NOOB-3301 <sepic466@gmail.com>
2025-01-14 22:13:39 +0530

    api keys

--- backend/utils/cloudinary.ts
+++ backend/utils/cloudinary.ts
@@ -5,9 +5,9 @@ import streamifier from 'streamifier'
 
 // Cloudinary configuration
 cloudinary.config({
-  cloud_name: 'djfhwhtyy',
-  api_key: '944476654513192',
-  api_secret: 'AbbLeYlaOpNfB1lHWeJACHmJGlg',
+  cloud_name: 'your_cloudinary_config',
+  api_key: 'your_cloudinary_config',
+  api_secret: 'your_clodinary_config',
   secure: true,
 });
 
